9b3402c324d4a7e800cc6df6f18481e0b6f0008d
dy <944801244@qq.com>
2018-10-11 10:11:10 +0800

    Spring Boot Admin Client

---
+++ demo_for_admin/src/main/java/com/example/demo/DemoForAdminApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class DemoForAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForAdminApplication.class, args);
+	}
+}
