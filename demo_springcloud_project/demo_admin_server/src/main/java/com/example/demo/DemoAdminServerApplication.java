348add042c4ad447cf75b2bb3f7322d3fa10b05c
dy <944801244@qq.com>
2018-10-11 17:59:42 +0800

    Spring Boot Admin Security

--- demo_springcloud_project/demo_admin_server/src/main/java/com/example/demo/DemoAdminServerApplication.java
+++ demo_springcloud_project/demo_admin_server/src/main/java/com/example/demo/DemoAdminServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
@@ -21,22 +22,44 @@ public class DemoAdminServerApplication {
 		SpringApplication.run(DemoAdminServerApplication.class, args);
 	}
 	
+//	@Configuration
+//	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+//	    @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//
+//	    	http.formLogin().loginPage("/login")
+//	        	.loginProcessingUrl("/login")
+//	        	.permitAll();
+//	        http.logout().logoutUrl("/logout");
+//	        http.csrf().disable();
+//	        http.authorizeRequests()
+//	        .antMatchers("/login.html", "/**/**.css", "/img/**", "/third-party/**")
+//	        	.permitAll();
+//	        http.authorizeRequests()
+//	        	.antMatchers("/**").authenticated();
+//	        http.httpBasic();
+//	    }
+//	}
+	
 	@Configuration
 	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		
 	    @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 
-	    	http.formLogin().loginPage("/login")
-	        	.loginProcessingUrl("/login")
-	        	.permitAll();
-	        http.logout().logoutUrl("/logout");
-	        http.csrf().disable();
-	        http.authorizeRequests()
-	        .antMatchers("/login.html", "/**/**.css", "/img/**", "/third-party/**")
-	        	.permitAll();
-	        http.authorizeRequests()
-	        	.antMatchers("/**").authenticated();
-	        http.httpBasic();
+	    	SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers("/assets/**").permitAll()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage("/login").successHandler(successHandler).and()
+                    .logout().logoutUrl("/logout").and()
+                    .httpBasic().and()
+                    .csrf().disable();
 	    }
 	}
+
 }
